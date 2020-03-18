5a7973824b47f14159ac73acf9b87f2ccbdaecac
7iomka <7iomka@Mac-mini-Daniil.local>
2020-03-18 03:48:31 +0200

    auth page demo

---
+++ client/src/pages/auth/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Auth = () => {
+  return (
+    <div className="auth-page">
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-6 offset-md-3 col-xs-12">
+            <h1 className="text-xs-center">Login</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
--- client/src/routes.js
+++ client/src/routes.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import GlobalFeed from 'pages/globalFeed';
 import Article from 'pages/article';
+import Auth from 'pages/auth';
 
 export default () => {
   return (
     <Switch>
-      <Route path="/" component={GlobalFeed} exact></Route>
-      <Route path="/articles/:slug" component={Article} exact></Route>
+      <Route path="/" component={GlobalFeed} exact />
+      <Route path="/articles/:slug" component={Article} />
+      <Route path="/login" component={Auth} />
+      <Route path="/register" component={Auth} />
     </Switch>
   );
 };
